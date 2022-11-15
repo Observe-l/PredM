83ec5606ed11ed8b8d34ab4bd014f5de5e9ab70e
Weihao.Liu <hitliuweihao@gmail.com>
2022-11-15 10:10:47 +0800

    Some result

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 python3 baseline.py --nogui -n 8 -c 0.5 -b 2 -m 1
 python3 baseline.py --nogui -n 8 -c 0.5 -b 2 -m 1 --repair
 
-python3 baseline.py --nogui -n 8 -c 0.5 -b 3 -m 6
-python3 baseline.py --nogui -n 8 -c 0.5 -b 3 -m 6 --repair
+python3 baseline.py --nogui -n 8 -c 0.5 -b 3 -m 1
+python3 baseline.py --nogui -n 8 -c 0.5 -b 3 -m 1 --repair
 
 python3 baseline.py --nogui -n 8 -c 0.5 -b 2 -m 1.5
 python3 baseline.py --nogui -n 8 -c 0.5 -b 2 -m 1.5 --repair
 
-python3 baseline.py --nogui -n 8 -c 0.5 -b 2 -m 1.5
-python3 baseline.py --nogui -n 8 -c 0.5 -b 2 -m 1.5 --repair
+python3 baseline.py --nogui -n 8 -c 0.5 -b 3 -m 1.5
+python3 baseline.py --nogui -n 8 -c 0.5 -b 3 -m 1.5 --repair
 
 python3 baseline.py --nogui -n 12 -c 0.5 -b 2 -m 1
 python3 baseline.py --nogui -n 12 -c 0.5 -b 2 -m 1 --repair
