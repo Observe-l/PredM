304de38aba49b1d54c00cf93c0ff3ffbc8691bbc
Weihao.Liu <hitliuweihao@gmail.com>
2022-11-15 22:23:34 +0800

    Add sensor readings

--- run.sh
+++ run.sh
@@ -15,4 +15,4 @@ python3 baseline.py --nogui -n 12 -c 0.5 -b 2 -m 1
 python3 baseline.py --nogui -n 12 -c 0.5 -b 2 -m 1 --repair
 
 python3 baseline.py --nogui -n 12 -c 0.5 -b 3 -m 1
-python3 baseline.py --nogui -n 12 -c 0.5 -b 3 -m 1 --rapair
+python3 baseline.py --nogui -n 12 -c 0.5 -b 3 -m 1 --repair
